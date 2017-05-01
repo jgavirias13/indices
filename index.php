23f508f6c42e84a7bc02afbb6a67906e3c502163
Dagc1496 <dagc_1496@hotmail.com>
2017-05-01 13:49:06 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+</head>
+<body>
+    <form method="GET" action="procesa.php">
+        Buscar: <input type="text" name="palabra" />
+        <br />
+        <input type="submit" value="Iniciar busqueda"/>
+    </form>
+</body>
+</html>
