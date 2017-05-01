e7c819747fe7f7c67ee5c38b2afc9528f915d824
Juan Pablo Gaviria+ <jgavirias13@gmail.com>
2017-05-01 01:51:51 -0400

    Cambios en indice para salir a mongo

--- src/indice.py
+++ src/indice.py
@@ -1,11 +1,18 @@
 from mrjob.job import MRJob
 from mrjob.step import MRStep
+from pymongo import MongoClient
 import os
+import unicodedata
 import string, re
+
+mongoclient = MongoClient('localhost',27017)
+db = mongoclient.jgaviri6
+coleccion = db.palabras
+
 
 def eliminarTildes(cadena):
     s = ''.join((c for c in unicodedata.normalize('NFD',unicode(cadena)) if unicodedata.category(c) != 'Mn'))
-    return s.decode()
+    return s
 
 class Contador(MRJob):
     def mapper(self,name,line):
@@ -20,7 +27,21 @@ class Contador(MRJob):
     def reducer(self, word, cant):
         rep = sum(cant)
         l = list(word)
-        yield word[0],(word[1],rep)
+        yield word[0],(rep,word[1])
+
+    def reducer2(self, word, doc):
+        l = list(doc)
+        documentos = sorted(l)
+        if len(documentos) > 5:
+            documentos = [l[0],l[1],l[2],l[3],l[4]]
+        coleccion.insert({"palabra":word,"documentos":l})
+        yield word,l
+    
+    def steps(self):
+        return [
+            MRStep(mapper=self.mapper,reducer=self.reducer),
+            MRStep(reducer=self.reducer2)
+        ]
 
 if __name__ == '__main__':
     Contador.run()
